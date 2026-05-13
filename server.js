02ee362b4263dfba5824c1d3822f23802d475d59
minkoulou <minkouloubenoit431@gmail.com>
2026-05-13 03:43:51 +0100

    feat: CRUD complet de blog avec expressJS

--- server.js
+++ server.js
@@ -4,8 +4,59 @@ const app=express();
 const PORT=process.env.PORT;
 app.use(express.json());
 
-app.get('/',(req,res)=>{
-    res.send('mon premier serveur avec express')
+const blog=[
+    {id:1,titre:"cameroun",descripion:"cameroun pays de paix"},
+
+    {id:2,titre:"nigeria",description:"peninsulle de bakasi"}
+]
+
+app.get('/blogs',(req,res)=>{
+    res.json(blog)
+})
+
+app.get('/blogs/:id',(req,res)=>{
+     const id=Number(req.params.id);
+    const newblog=blog.find(b => b.id === id);
+    if(!newblog){
+        res.status(404);
+        res.json({message:"blog non retrouve"});
+    }else{
+      res.status(200)
+      res.json(newblog);
+         }
+})
+
+app.post('/blogs',(req,res)=>{
+     const {titre,description} = req.body;
+     if(!titre || !description){
+        res.status(404).json({message:'champs manquant'})
+     }else{
+     const nouveaublog={
+        id:blog.length + 1,
+        titre,
+        description
+     }
+
+     blog.push(nouveaublog);
+     res.status(201).json(nouveaublog)
+    }
+})
+
+app.put('/blogs/:id',(req,res)=>{
+    const {titre,description}=req.body
+    const id=parseInt(req.params.id);
+    const index=blog.find(b => b.id === id)
+    blog[index]={...req.body}
+
+    res.status(200).json('ressource mis a jour')
+
+})
+
+app.delete('/blogs/:id',(req,res)=>{
+    const id=req.params.id;
+    const index=blog.find(b => b.id === id)
+    blog.splice(index,1);
+    res.status(200).json({message:'blog supprime avec success'})
 })
 
 app.listen(PORT, ()=>{
